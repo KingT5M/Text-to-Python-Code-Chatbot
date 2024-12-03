7375103ddc919d979c4c7c59ac36729019f2ea4c
THE PENTAGON MOVEMENT. <134065998+KingT5M@users.noreply.github.com>
2024-12-04 01:26:03 +0300

    Successfully split the data into train, validation and test splits

--- data.py
+++ data.py
@@ -1,10 +1,20 @@
 #import the necessary libraries
-from datasets import load_dataset
+from datasets import load_dataset, DatasetDict
+from transformers import AutoTokenizer
 
-#download and print dataset
+#download the dataset
 dataset = load_dataset("iamtarun/python_code_instructions_18k_alpaca")
-print (dataset)
-
+   
+#PREPROCESS THE DATA
+#create the train, validation and test splits
+dataset_train_test = dataset['train'].train_test_split(test_size=0.2, seed = 42) #20% for testing. Seed ensures that every time you run the code, the data will be shuffled in the same way.
+dataset_train_validation = dataset['train'].train_test_split(test_size=0.1, seed = 42)#8% for validation
+#reconstruct the DatasetDict
+dataset = DatasetDict({
+    'train': dataset_train_test['train'],
+    'validation': dataset_train_validation['test'],
+    'test': dataset_train_test['test']
+})
 #check if dataset has train, validation and test splits
 if 'train' in dataset:
     print("Dataset has a train split")
@@ -12,5 +22,6 @@ if 'validation' in dataset:
     print("Dataset has a validation split")
 if 'test' in dataset:
     print("Dataset has a test split")
-    
-#preprocess the data
+#print dataset
+print (dataset)
+
